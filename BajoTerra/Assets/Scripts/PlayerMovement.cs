3cc86a41bd33c762b412775a0077ea8e1485f5cc
OlavMartos <olav.martos.7e4@itb.cat>
2023-11-21 19:52:24 +0100

    Mage Anims
    Mage Sprite, Mage Idle Anim Blend Tree, Mage Walk Blend Tree, Mage Attack Blend Tree a la mitad

--- BajoTerra/Assets/Scripts/PlayerMovement.cs
+++ BajoTerra/Assets/Scripts/PlayerMovement.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Net.NetworkInformation;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -8,10 +9,13 @@ public class PlayerMovement : MonoBehaviour
     public float speed;
     private Vector2 movement;
     private Rigidbody2D rb;
+    private Animator animator;
+    private Vector2 direction = new Vector2();
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     private void OnMovement(InputValue value)
@@ -22,5 +26,17 @@ public class PlayerMovement : MonoBehaviour
     private void FixedUpdate()
     {
         rb.MovePosition(rb.position + movement * speed * Time.fixedDeltaTime);
+
+        if(movement.x != 0 || movement.y != 0)
+        {
+            animator.SetFloat("posX", movement.x);
+            animator.SetFloat("posY", movement.y);
+
+            animator.SetBool("isWalking", true);
+        }
+        else
+        {
+            animator.SetBool("isWalking", false);
+        }
     }
 }
