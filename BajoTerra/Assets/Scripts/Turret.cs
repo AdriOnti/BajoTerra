e4575fb0c2681de66fcf10f81911409a12787d9b
OlavMartos <olav.martos.7e4@itb.cat>
2023-11-30 17:57:19 +0100

    Turret End

--- BajoTerra/Assets/Scripts/Turret.cs
+++ BajoTerra/Assets/Scripts/Turret.cs
@@ -7,28 +7,38 @@ public class Turret : MonoBehaviour
     public float visionRange = 5f;
     public float rotateSpeed = 3f;
     public float shotSpeed = 50f;
-    public Transform player;
-    public GameObject projectile;
+
+
+    private GameObject player;
+    private GameObject projectile;
+
+
     public int projectileQuantity;
+    public float bulletLifeTime;
+    public float timeBetweenShots;
 
     private List<Transform> pool;
+    private Animator animator;
 
     void Start()
     {
-        // Inicializar el pool de balas
+        animator = GetComponent<Animator>();
+        player = GameObject.FindGameObjectWithTag("Player");
+        projectile = GameObject.Find("TurretBullet");
+        
         InstantiatePoolItem();
     }
 
     void Update()
     {
-        // Verificar si el jugador está dentro del rango de visión
-        if (Vector2.Distance(transform.position, player.position) <= visionRange)
+        if (Vector2.Distance(transform.position, player.transform.position) <= visionRange)
         {
-            // Girar hacia el jugador
             RotatePlayer();
-
-            // Disparar al jugador
             StartCoroutine(Shot());
+        }
+        else
+        {
+            animator.SetBool("isShooting", false);
         }
     }
 
@@ -46,7 +56,7 @@ public class Turret : MonoBehaviour
 
     void RotatePlayer()
     {
-        Vector2 direction = player.position - transform.position;
+        Vector2 direction = player.transform.position - transform.position;
         float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
         Quaternion rotate2Player = Quaternion.AngleAxis(angle, Vector3.forward);
         transform.rotation = Quaternion.Slerp(transform.rotation, rotate2Player, Time.deltaTime * rotateSpeed);
@@ -54,29 +64,29 @@ public class Turret : MonoBehaviour
 
     IEnumerator Shot()
     {
-        // Buscar una bala inactiva en el pool
-        Transform shotTransform = pool.Find(b => !b.gameObject.activeSelf);
-
-        // Si encontramos una bala inactiva, la activamos y la disparamos
-        if (shotTransform != null)
+        animator.SetBool("isShooting", true);
+        foreach (Transform shotTransform in pool)
         {
-            shotTransform.position = transform.position;
-            shotTransform.rotation = transform.rotation;
-            shotTransform.gameObject.SetActive(true);
+            if (!shotTransform.gameObject.activeSelf && animator.GetBool("isShooting"))
+            {
+                
+                shotTransform.position = transform.position;
+                shotTransform.rotation = transform.rotation;
+                shotTransform.gameObject.SetActive(true);
 
-            // Obtener la dirección hacia el jugador
-            Vector2 direction = player.position - transform.position;
+                Vector2 direction = player.transform.position - transform.position;
 
-            yield return new WaitForSeconds(direction.magnitude);
+                Rigidbody2D rbShot = shotTransform.GetComponent<Rigidbody2D>();
+                rbShot.velocity = direction.normalized * shotSpeed;
 
-            // Configurar la velocidad de la bala
-            Rigidbody2D rbShot = shotTransform.GetComponent<Rigidbody2D>();
-            rbShot.velocity = direction.normalized * shotSpeed;
-
-            // Desactivar la bala después de un tiempo (ajusta según tus necesidades)
-            StartCoroutine(DesactivarBala(shotTransform.gameObject, 0.5f));
+                StartCoroutine(DesactivarBala(shotTransform.gameObject, bulletLifeTime));
+            }
+            // Esperar antes de disparar la siguiente bala
+            yield return new WaitForSeconds(timeBetweenShots); // Ajusta según tus necesidades
+            animator.SetBool("isShooting", false);
         }
     }
+
 
     IEnumerator DesactivarBala(GameObject shot, float waitTime)
     {
