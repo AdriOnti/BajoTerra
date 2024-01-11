67ac86312a884bc3d0a5ee55a6df6a80c37e16e9
AdriOnti <adrian.ontiveros.7e4@itb.cat>
2024-01-11 20:05:49 +0100

    room doors

--- BajoTerra/Assets/Scripts/Paths.cs
+++ BajoTerra/Assets/Scripts/Paths.cs
@@ -2,10 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Paths : MonoBehaviour
+public class Paths : RoomManager
 {
-    string path;
-
     // Start is called before the first frame update
     void Start()
     {
--- BajoTerra/Assets/Scripts/RoomManager.cs
+++ BajoTerra/Assets/Scripts/RoomManager.cs
@@ -3,13 +3,42 @@ using UnityEngine;
 
 public class RoomManager : MonoBehaviour
 {
-    public List<GameObject> rooms;
-    private string path; 
+    protected Transform roomBoard;
+    protected Transform roomParent;
+    protected List<Transform> rooms = new List<Transform>();
+    private string path;
 
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-        
+        switch (tag)
+        {
+            case "NDoor":
+                path = "N";
+                break;
+            case "SDoor":
+                path = "S";
+                break;
+            case "EDoor":
+                path = "E";
+                break;
+            case "ODoor":
+                path = "O";
+                break;
+        }
+    }
+
+    void Awake()
+    {
+        roomBoard = GameObject.FindGameObjectWithTag("RoomManager").transform;
+        roomParent = GameObject.FindGameObjectWithTag("Rooms").transform;
+        foreach (Transform child in roomParent)
+        {
+            foreach (Transform child2 in child)
+            {
+                rooms.Add(child2);
+            }
+        }
     }
 
     // Update is called once per frame
@@ -33,6 +62,25 @@ public class RoomManager : MonoBehaviour
                 break;
             case "O":
                 break;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        SetChildrenPositions();
+    }
+
+    void SetChildrenPositions()
+    {
+        int i = 0;
+        foreach (Transform child in roomBoard)
+        {
+            foreach (Transform child2 in child)
+            {
+                rooms[i].SetParent(child2);
+                rooms[i].position = child2.position;
+                i++;
+            }
         }
     }
 }
