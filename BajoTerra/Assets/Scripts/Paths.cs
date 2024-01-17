1fe86037cc2734dfc86d471cf03c31bcb46bf3a3
AdriOnti <adrian.ontiveros.7e4@itb.cat>
2024-01-18 00:52:44 +0100

    new room system

--- BajoTerra/Assets/Scripts/Paths.cs
+++ BajoTerra/Assets/Scripts/Paths.cs
@@ -1,9 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 
-public class Paths : RoomManager
+public class Paths : MonoBehaviour
 {
+    protected bool usedDoor;
+    protected Transform player;
+    protected string path;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +19,36 @@ public class Paths : RoomManager
     void Update()
     {
         
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            player = collision.gameObject.transform;
+
+            switch (tag)
+            {
+                case "N":
+                    path = "S";
+                    player.position = new Vector3(player.position.x, player.position.y + 4.5f, player.position.z);
+                    break;
+                case "S":
+                    path = "N";
+                    player.position = new Vector3(player.position.x, player.position.y - 4.5f, player.position.z);
+                    break;
+                case "E":
+                    path = "O";
+                    player.position = new Vector3(player.position.x + 3f, player.position.y, player.position.z);
+                    break;
+                case "O":
+                    path = "E";
+                    player.position = new Vector3(player.position.x - 3f, player.position.y, player.position.z);
+                    break;
+            }
+
+            if (!usedDoor) RoomManager.Instance.SetRoom(path);
+            usedDoor = true;
+        }
     }
 }
--- BajoTerra/Assets/Scripts/RoomManager.cs
+++ BajoTerra/Assets/Scripts/RoomManager.cs
@@ -1,4 +1,6 @@
+using System;
 using System.Collections.Generic;
+using UnityEditor.PackageManager;
 using UnityEngine;
 
 public class RoomManager : MonoBehaviour
@@ -7,23 +9,48 @@ public class RoomManager : MonoBehaviour
     protected Transform roomParent;
     protected List<Transform> boardSpaces = new List<Transform>();
     protected List<Transform> rooms = new List<Transform>();
-    protected string path;
-    protected Transform player;
+    public string[,] roomBoard2 = new string[6,9];
+    protected Dictionary<boardRooms, Transform> roomsDictionary = new();
+    public static RoomManager Instance;
+
+    public enum boardRooms
+    {
+        R0_0, R0_1, R0_2, R0_3, R0_4, R0_5, R0_6, R0_7, R0_8, R0_9,
+        R1_0, R1_1, R1_2, R1_3, R1_4, R1_5, R1_6, R1_7, R1_8, R1_9,
+        R2_0, R2_1, R2_2, R2_3, R2_4, R2_5, R2_6, R2_7, R2_8, R2_9,
+        R3_0, R3_1, R3_2, R3_3, R3_4, R3_5, R3_6, R3_7, R3_8, R3_9,
+        R4_0, R4_1, R4_2, R4_3, R4_4, R4_5, R4_6, R4_7, R4_8, R4_9,
+        R5_0, R5_1, R5_2, R5_3, R5_4, R5_5, R5_6, R5_7, R5_8, R5_9
+    }
 
     // Start is called before the first frame update
     private void Start()
     {
-
+        
     }
 
     void Awake()
     {
+        for (int i = 0; i < roomBoard2.GetLength(0); i++)
+        {
+            for (int j = 0; j < roomBoard2.GetLength(1); j++)
+            {
+                roomBoard2[i, j] = "0";
+            }
+        }
+
         roomBoard = GameObject.FindGameObjectWithTag("RoomManager").transform;
         roomParent = GameObject.FindGameObjectWithTag("Rooms").transform;
 
         foreach (Transform child in roomBoard)
         {
             boardSpaces.Add(child);
+            bool sucess;
+            if (sucess = Enum.TryParse(child.gameObject.name, out boardRooms value))
+            {
+                roomsDictionary.Add(value, child);
+                Debug.Log(sucess);
+            }
         }
 
         foreach (Transform child in roomParent)
@@ -41,37 +68,7 @@ public class RoomManager : MonoBehaviour
         
     }
 
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        if (collision.gameObject.tag == "Player")
-        {
-            player = collision.gameObject.transform;
-        }
-
-        switch (tag)
-        {
-            case "N":
-                path = "S";
-                player.position = new Vector3(player.position.x, player.position.y + 4.5f, player.position.z);
-                break;
-            case "S":
-                path = "N";
-                player.position = new Vector3(player.position.x, player.position.y - 4.5f, player.position.z);
-                break;
-            case "E":
-                path = "O";
-                player.position = new Vector3(player.position.x + 3f, player.position.y, player.position.z);
-                break;
-            case "O":
-                path = "E";
-                player.position = new Vector3(player.position.x - 3f, player.position.y, player.position.z);
-                break;
-        }
-
-        SetRoom();
-    }
-
-    void SetRoom()
+    public void SetRoom(string path)
     {
         Debug.Log(path);
 
@@ -79,11 +76,24 @@ public class RoomManager : MonoBehaviour
         {
             if (rooms[i].name.Contains(path))
             {
-                Debug.Log("a");
                 rooms[i].SetParent(boardSpaces[3]);
                 rooms[i].position = boardSpaces[3].position;
                 break;
             }
         }
+    }
+
+    string ShowRoomboard()
+    {
+        string matrix = "";
+        for (int i = 0; i < roomBoard2.GetLength(0); i++)
+        {
+            for (int j = 0; j < roomBoard2.GetLength(1); j++)
+            {
+                matrix += roomBoard2[i, j] + ",";
+            }
+            matrix += "\n";
+        }
+        return matrix;
     }
 }
